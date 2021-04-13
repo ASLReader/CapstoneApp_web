784d733ace78d3bca00474c08769aafc672be325
Rohaan Williams <rwill046@uottawa.ca>
2021-04-12 21:47:07 -0400

    Upadte Unit Test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,12 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('capston-app');
   });
-
-  /*it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('capston-app app is running!');
-  });
-  */
 });
